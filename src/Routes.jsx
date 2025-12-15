bd5e7d38bbd8c721496827ad746e48a590b4c54e
devjorgeluis <jorgencnc0608@gmail.com>
2025-12-16 06:52:29 +0800

    remove unused codes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Casino from "./pages/Casino";
 import LiveCasino from "./pages/LiveCasino";
@@ -13,7 +12,6 @@ export default function AppRoutes() {
         <Routes>
             <Route element={<Layout />}>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/casino" element={<Casino />} />
                 <Route path="/live-casino" element={<LiveCasino />} />
